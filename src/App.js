5779b607dbae0cb6d4afa5c4891c71324ba2fb63
hukuuu <hukuuu@gmail.com>
2019-11-05 23:09:25 +0200

    lobby page prototyping

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import './App.css'
-import { Router, Route } from 'react-router-dom'
+import { Switch, Router, Route, Redirect } from 'react-router-dom'
 import history from './history'
-import Home from './Pages/Home'
+import Lobby from './Pages/Lobby'
 import Login from './Pages/Login'
+import Settings from './Pages/Settings'
 import PrivateRoute from './Authentication/PrivateRoute'
 
 function App() {
   return (
     <Router history={history}>
-      <PrivateRoute exact path="/">
-        <Home />
-      </PrivateRoute>
-      <Route path="/login" exact component={Login} />
+      <Switch>
+        <Route path="/login" exact component={Login} />
+        <PrivateRoute exact path="/">
+          <Lobby />
+        </PrivateRoute>
+        <PrivateRoute exact path="/settings">
+          <Settings />
+        </PrivateRoute>
+        <Redirect to="/" />
+      </Switch>
     </Router>
   )
 }
